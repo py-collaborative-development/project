f8f7bfcaf788da134fe5bd9e05f04a07da2f628e
imArtyfact <rtgenius0702@gmail.com>
2019-04-23 13:52:47 +0300

    Color scheme added, design improved

--- src/constants.py
+++ src/constants.py
@@ -2,3 +2,14 @@ HEIGHT = 8
 WIDTH = 8
 BOMBS = 5
 BTN_SIZE_RATIO = 32
+
+COLORS = {
+    1: 'purple',
+    2: 'green',
+    3: 'red',
+    4: 'yellow',
+    5: 'brown',
+    6: 'orange',
+    7: 'pink',
+    8: 'black'
+}
